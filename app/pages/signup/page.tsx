a67376408bad125d6a941a22806a72ac43fe960e
super <superbear3000@gmail.com>
2024-03-17 00:27:59 -0400

    Fixes

--- app/pages/signup/page.tsx
+++ app/pages/signup/page.tsx
@@ -73,7 +73,7 @@ function SignupContainer() {
             setIsLoading(true);
             // Create model
             try {
-                const res = await axios.post(`${process.env.VERCEL_URL}api/register`, regData)
+                const res = await axios.post(`cs-match-7jwaikqu9-geoffrey-dewittys-projects.vercel.app/api/register`, regData)
                 .then(() => signIn('credentials', regData))
                 
                 .finally(() => setIsLoading(false))
