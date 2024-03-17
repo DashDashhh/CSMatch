9f94cdf17ae76d5687a6b544e0f78e49be7b9f3b
super <superbear3000@gmail.com>
2024-03-16 22:41:50 -0400

    fixes

--- app/pages/create/page.tsx
+++ app/pages/create/page.tsx
@@ -12,7 +12,7 @@ import { useState, useEffect } from 'react';
 
 import { useRouter } from 'next/navigation';
 
-import axios from 'axios';
+
 
 const InternBio = () => {
 
--- app/pages/internlist/page.tsx
+++ app/pages/internlist/page.tsx
@@ -7,10 +7,30 @@ import InternCard from "@/app/(site)/components/interncard";
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react';
 
+// API GET DATA FROM MONGO
+
+import {NextResponse} from "next/server";
+
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+    try {
+        const cards = await prisma.userCardInfo.findMany();
+        return new NextResponse(JSON.stringify(cards))
+    } catch(error) {
+        return new NextResponse('error')
+    }
+
+}
+
+
+
 
 async function getData() {
-    const res = await fetch(`${process.env.VERCEL_URL}/api/cards`, {cache: "no-store"});
-    if(!res.ok) console.log('error');
+    const res = await GET()
     return res.json();
 }
 
