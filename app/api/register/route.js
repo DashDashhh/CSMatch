a31530d0e8f81ecdfd33914aa2fcf673d237d8bf
super <superbear3000@gmail.com>
2024-03-19 00:24:57 -0400

    CHANGES

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -49,7 +49,7 @@ export async function POST(request) {
           email: email,
           phoneNumber: '',
           socials: '',
-          desription: '',
+          description: '',
           userId: user.id
         }
       })
