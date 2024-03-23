39de1a1ac13f96907fcf437846a740c9d6003032
super <superbear3000@gmail.com>
2024-03-23 19:53:13 -0400

    CHANGES

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,13 @@ import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
   pages: {
-    signIn: `${process.env.VERCEL_URL}/pages/login`,
+    signIn: `/pages/login`,
   },
 });
 
 export const config = { 
   matcher: [
-    `${process.env.VERCEL_URL}/pages/create`,
-    `${process.env.VERCEL_URL}/pages/myprofile`
+    `/pages/create`,
+    `/pages/myprofile`
   ]
 };
