0f4b3ff4726a539439d9e26688950503e8fa2d5a
super <superbear3000@gmail.com>
2024-04-11 20:48:48 -0400

    CHANGES

--- app/pages/internlist/bio/[slug]/page.tsx
+++ app/pages/internlist/bio/[slug]/page.tsx
@@ -47,7 +47,7 @@ const InternBio = async({ params }: any) => {
                     <a id="resume__download" href={bio.resumeUrl}>Download My Resume Here!</a>
 
                     <div className="button__wrapper">
-                        <a className="button" id="confirm" onClick={red}>Download Resume</a> 
+                        <a className="button" id="confirm" onClick={() => red(bio.resumeUrl)}>Download Resume</a> 
                     </div>
 
                     <div className="information">
